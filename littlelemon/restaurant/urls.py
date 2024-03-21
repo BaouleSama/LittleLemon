f3a6f4c057672cf183fddd64d3a4381edc925358
Kouadio NDAH <ndahcharles@outlook.com>
2024-03-21 00:01:23 -0500

    Create Bookin and Menu API

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
+# User rest_framework route.
+# Eliminate the need of creating root path
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r"tables", views.BookingSet)
+
 urlpatterns = [
-    path("", views.index, name='index')
+    # add the rest_framework routr the partterns
+    path("", include(router.urls)),
+    path('restaurant/booking/', include(router.urls)),
+
+    path("", views.index, name='index'),
+    path("menu/", views.MenuItemsView.as_view()),
+    path("menu/<int:pk>", views.SingleMenuView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Booking, Menu
+from django.contrib.auth.models import User
+from .serializers import MenuSerializer, UserSerializer, BookingSerializer
 
 # Create your views here.
 
 
 def index(request):
     return render(request, "index.html", {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class SingleMenuView(generics.RetrieveAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingSet(viewsets.ModelViewSet):
+    serializer_class = BookingSerializer
+    queryset = Booking.objects.all()
+
+
+# Create a viewSet to create user
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
